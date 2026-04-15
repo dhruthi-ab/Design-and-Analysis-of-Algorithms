c40ad820f9f7fa71c8d86ad6839c2a3e42fe08d3
Dhruthi.P <dhrudhruthi22@gmail.com>
2026-04-15 10:29:10 +0530

    knapsack.c
    This program implements the 0/1 Knapsack Problem using Dynamic Programming in C.
    The knapsack problem is a classic optimization problem where you are given:
    - A set of n objects, each with a weight and a profit (value).
    - A knapsack with a maximum capacity W.
    The goal is to maximize the total profit by selecting a subset of objects such that the total weight does not exceed the knapsack’s capacity.

---
+++ knapsack.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+int max(int a,int b)
+{
+    if(a>b)
+        return a;
+    else
+        return b;
+}
+int main()
+{
+    int n,W,w[10],p[10],i,j,V[10][10],x[10]={10};
+    printf("\n Read number of objects:");
+    scanf("%d",&n);
+    printf("\n Read knapsack capacity:");
+    scanf("%d",&W);
+    printf("\n Read weight of objects:");
+    for(i=1;i<=n;i++)
+        scanf("%d",&w[i]);
+    printf("\n Read the profits of object:");
+    for(i=1;i<=n;i++)
+        scanf("%d",&p[i]);
+    for(i=0;i<=n;i++)
+    {
+        for(j=0;j<=W;j++)
+        {
+            if(i==0 || j==0)
+                V[i][j]=0;
+            else if(j-w[i]<0)
+                V[i][j]=V[i-1][j];
+            else
+                V[i][j]=max(V[i-1][j],V[i-1][j-w[i]]+p[i]);
+                printf("%d\t",V[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
